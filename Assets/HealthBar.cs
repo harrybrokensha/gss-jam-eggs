8241f82d1e1261a5ed83b62f43eb383db86155d8
Harry Brokensha <contact@harrybrokensha.com>
2020-03-15 20:01:38 +0000

    First HTTPS test

---
+++ Assets/HealthBar.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image ImgHealthBar;
+
+   
+
+    public float MaxTime;
+    public float Counter;
+    public Text CounterText;
+    public ShadowDetectorChecker MyShadowChecker;
+    public GameObject myObject;
+    private bool isCreated;
+    private float FinalValue;
+
+
+    void Start()
+    {
+        isCreated = false;
+    }
+
+    void Update()
+    {
+        if (MyShadowChecker.ItBurns)
+        {
+            Counter += Time.deltaTime;
+
+            if (Counter >= MaxTime )//&& !isCreated)
+            {
+                Instantiate(myObject, MyShadowChecker.transform.position, MyShadowChecker.transform.rotation);
+                //Counter = MaxTime / 2f;
+                //isCreated = true;
+                Counter = 0f;
+            }
+
+
+        }
+        else
+        {
+            isCreated = false;
+            Counter -= 0.5f * Time.deltaTime;
+            if (Counter <= 0f)
+            {
+                Counter = 0f;
+            }
+        }
+        FinalValue = 1.0f - (Counter / MaxTime);
+        ImgHealthBar.fillAmount = FinalValue;
+        CounterText.text = FinalValue.ToString("0");
+    }
+
+
+    
+}
---
+++ Assets/ShadowDetector/Demo/Scripts/ShadowDetectorChecker.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class ShadowDetectorChecker : MonoBehaviour {
+
+    public Text sensorBr;
+    public Transform eyeEnabled;
+    public Transform eyeDisabled;
+    private ShadowDetector sd;
+    private Image img;
+    public bool ItBurns;
+   
+   
+
+
+    void Start () {
+        sd = transform.GetComponent<ShadowDetector>();
+        img = eyeEnabled.GetComponent<Image>();
+      
+    }
+	
+	void Update () {
+
+        
+
+        if (sd.hidden)
+        {
+            eyeEnabled.gameObject.SetActive(false);
+            eyeDisabled.gameObject.SetActive(true);
+
+            ItBurns = false;
+        }
+        else
+        {
+            eyeEnabled.gameObject.SetActive(true);
+            eyeDisabled.gameObject.SetActive(false);
+           
+            // Do smth when exposed to sunlight
+            ItBurns = true;
+
+        }
+        img.color = new Color(img.color.r, img.color.g, img.color.b, sd.bright / sd.maxShadowBright);
+        sensorBr.text = "Sensor Bright: " + System.Math.Round(sd.bright, 2);
+
+        if (ItBurns)
+        {
+           
+            
+        }
+
+        
+    }
+
+}
